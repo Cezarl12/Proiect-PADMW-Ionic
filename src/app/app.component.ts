608994d9e0b075b8cfef6d694b2bac4791c8dffa
Cezarl12 <cezar.lauran15@gmail.com>
2026-03-04 17:49:33 +0200

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
-import { NavigationPage } from "./pages/navigation/navigation.page";
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet, NavigationPage],
+  imports: [IonApp, IonRouterOutlet],
 })
 export class AppComponent {
   constructor() { }
