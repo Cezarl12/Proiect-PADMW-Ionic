efffb86afb10802526965316cee14e976c7704f4
Cezarl12 <cezar.lauran15@gmail.com>
2026-03-11 17:59:24 +0200

    security issues

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCvp4FUoOr7ZzWR2rMcmUGIJtfNjA-qSbs",
+    authDomain: "recipehub-535f1.firebaseapp.com",
+    projectId: "recipehub-535f1",
+    storageBucket: "recipehub-535f1.firebasestorage.app",
+    messagingSenderId: "381635611300",
+    appId: "1:381635611300:web:c43d5a6675f599cd273431",
+    measurementId: "G-89SNY01JMD"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCvp4FUoOr7ZzWR2rMcmUGIJtfNjA-qSbs",
+    authDomain: "recipehub-535f1.firebaseapp.com",
+    projectId: "recipehub-535f1",
+    storageBucket: "recipehub-535f1.firebasestorage.app",
+    messagingSenderId: "381635611300",
+    appId: "1:381635611300:web:c43d5a6675f599cd273431",
+    measurementId: "G-89SNY01JMD"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
